abccc826eb675c2dbbbe0ed2d861a68f31c1730b
Sig Lange <sig.lange@gmail.com>
2023-12-14 11:01:50 -0800

    flakes update

--- shell.nix
+++ shell.nix
@@ -1,8 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs;
+# let
+#      pkgs = import (builtins.fetchGit {
+#          name = "old-hugo";
+#          url = "https://github.com/NixOS/nixpkgs/";
+#          ref = "refs/heads/nixpkgs-unstable";
+#          rev = "c8e344196154514112c938f2814e809b1ca82da1";
+#      }) {};
 
-mkShell {
+#      oldHugo = pkgs.hugo;
+# in
+#   with pkgs; mkShell {
+#     buildInputs = [
+#       oldHugo
+#     ];
+#   }
+
+with pkgs; mkShell {
   buildInputs = [
+        hugo
   ];
 }
