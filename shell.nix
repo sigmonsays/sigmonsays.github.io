39f6922cfa762816eddd6777a0cba004b407ef66
Sig Lange <sig.lange@gmail.com>
2023-12-14 12:44:08 -0800

    nix-direnv update

--- shell.nix
+++ shell.nix
@@ -1,21 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
-# let
-#      pkgs = import (builtins.fetchGit {
-#          name = "old-hugo";
-#          url = "https://github.com/NixOS/nixpkgs/";
-#          ref = "refs/heads/nixpkgs-unstable";
-#          rev = "c8e344196154514112c938f2814e809b1ca82da1";
-#      }) {};
-
-#      oldHugo = pkgs.hugo;
-# in
-#   with pkgs; mkShell {
-#     buildInputs = [
-#       oldHugo
-#     ];
-#   }
-
 with pkgs; mkShell {
   buildInputs = [
         hugo
