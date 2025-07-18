ad0cb135b604a624ad334f44893c38ead174d6a5
Sig Lange <sig.lange@gmail.com>
2025-07-17 21:27:06 -0700

    comments junk

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs; mkShell {
-  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
-  LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
+  # LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+  # LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
   buildInputs = [
     zs
     minify
