d3a8f7dd435a51a00cede718a577826d023526d0
Sig Lange <sig.lange@gmail.com>
2025-10-23 13:24:01 -0700

    publish

--- main.go
+++ main.go
@@ -19,8 +19,11 @@ import (
 )
 
 type SiteRender struct {
-	OutDir string
-	Site   *site.Site
+	Verbose       bool
+	includeDrafts bool
+	InDir         string
+	OutDir        string
+	Site          *site.Site
 }
 
 func (me *SiteRender) WriteComponent(ctx context.Context, c templ.Component, filename string) error {
@@ -36,7 +39,9 @@ func (me *SiteRender) WriteComponent(ctx context.Context, c templ.Component, fil
 	if err != nil {
 		return err
 	}
-	fmt.Printf("wrote %s\n", filename)
+	if me.Verbose {
+		fmt.Printf("wrote %s\n", filename)
+	}
 	return nil
 }
 
@@ -65,7 +70,26 @@ func (me *SiteRender) WritePage(ctx context.Context, page *site.PageMetadata, ti
 	return me.WriteComponent(ctx, comp, outfile)
 }
 
+func (me *SiteRender) WriteDirectoryIndexPage(ctx context.Context, inDir, outDir string) error {
+	relDir, err := filepath.Rel(me.InDir, inDir)
+	if err != nil {
+		return err
+	}
+	outfile := filepath.Join(me.OutDir, relDir, "index.html")
+	log.Printf("Write directory index %s", outfile)
+	title := fmt.Sprintf("directory %s", relDir)
+	_pages, err := GetPages(me.InDir, inDir, me.includeDrafts)
+	if err != nil {
+		return err
+	}
+	pages := site.SortPages(_pages)
+
+	comp := site.DirectoryIndex(*me.Site, title, pages)
+	return me.WriteComponent(ctx, comp, outfile)
+}
+
 type Options struct {
+	Verbose   bool
 	DevServer bool
 	InDir     string
 	OutDir    string
@@ -85,6 +109,7 @@ func main() {
 	flag.StringVar(&opts.OutDir, "out", opts.OutDir, "output directory")
 	flag.BoolVar(&opts.DevServer, "dev", opts.DevServer, "start dev server")
 	flag.BoolVar(&gen, "gen", gen, "generate and exit")
+	flag.BoolVar(&opts.Verbose, "verbose", opts.Verbose, "be verbose")
 	flag.StringVar(&opts.BindAddr, "bind", opts.BindAddr, "dev http server bind address")
 	flag.Parse()
 
@@ -115,13 +140,14 @@ type FileEntry struct {
 	Info fs.FileInfo
 }
 
-func GetPages(dir string, includeDrafts bool) ([]*site.PageMetadata, error) {
+func GetPages(rootdir, dir string, includeDrafts bool) ([]*site.PageMetadata, error) {
 	ret := make([]*site.PageMetadata, 0)
 	entries := make([]*FileEntry, 0)
 	walkfn := func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return nil
 		}
+
 		e := &FileEntry{
 			Path: path,
 			Info: info,
@@ -159,7 +185,7 @@ func GetPages(dir string, includeDrafts bool) ([]*site.PageMetadata, error) {
 
 		}
 
-		relPath, err := filepath.Rel(dir, ent.Path)
+		relPath, err := filepath.Rel(rootdir, ent.Path)
 		if err != nil {
 			fmt.Printf("ERROR: RelPath: %s", err)
 		}
@@ -176,8 +202,8 @@ func GetPages(dir string, includeDrafts bool) ([]*site.PageMetadata, error) {
 			Markdown:    bodyMD,
 			FrontMatter: fm,
 		}
-		fmt.Printf("Page %s Url:%q RelPath:%q\n",
-			page.Filename, page.Url, page.RelPath)
+		// fmt.Printf("Page %s Url:%q RelPath:%q\n",
+		// 	page.Filename, page.Url, page.RelPath)
 		ret = append(ret, page)
 	}
 
@@ -194,9 +220,23 @@ func genSite(opts *Options) error {
 		return err
 	}
 
-	pages, err := GetPages(inDir, opts.DevServer)
+	pages, err := GetPages(inDir, inDir, opts.DevServer)
 	if err != nil {
 		return err
+	}
+
+	// track directories and child object count so we can generate indexes
+	directories := make(map[string]int, 0)
+	for _, page := range pages {
+		dirname := filepath.Dir(page.Filename)
+		count, found := directories[dirname]
+		if !found {
+			count = 1
+		}
+		if dirname == "." || dirname == opts.InDir {
+			continue
+		}
+		directories[dirname] = count + 1
 	}
 
 	siteObj := &site.Site{
@@ -205,8 +245,11 @@ func genSite(opts *Options) error {
 	}
 
 	siteRender := &SiteRender{
-		OutDir: outDir,
-		Site:   siteObj,
+		Verbose:       opts.Verbose,
+		includeDrafts: opts.DevServer,
+		InDir:         opts.InDir,
+		OutDir:        opts.OutDir,
+		Site:          siteObj,
 	}
 
 	// build tag mapping
@@ -273,6 +316,17 @@ func genSite(opts *Options) error {
 			return err
 		}
 
+	}
+
+	// generate index page for directories
+	for directory, count := range directories {
+		outdir := filepath.Join(opts.OutDir, directory)
+		fmt.Printf("WIP: generate index for %s (%d children, outdir:%s)\n",
+			directory, count, outdir)
+		err := siteRender.WriteDirectoryIndexPage(ctx, directory, outdir)
+		if err != nil {
+			log.Printf("Error: generating dir index %s: %s", directory, err)
+		}
 	}
 
 	log.Printf("finished generating site")
