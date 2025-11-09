ba763a15aef23c2fc3be8f48515a152b01b36492
Sig Lange <sig.lange@gmail.com>
2025-11-08 22:20:27 -0800

    publish

--- site/footer_templ.go
+++ site/footer_templ.go
@@ -29,7 +29,7 @@ func Footer() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<footer style=\"padding:1rem;border-top:1px solid #ddd;margin-top:2rem\"><small>© Sigmonsays 2016</small></footer>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<footer style=\"padding:1rem;border-top:1px solid #ddd;margin-top:2rem\"><small>© Sigmonsays 2014</small></footer>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
