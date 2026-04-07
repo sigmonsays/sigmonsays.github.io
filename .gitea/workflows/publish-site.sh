d7f900ef8c16fe8aa2a3855f9a89a8c766a3fc0b
Sig Lange <sig.lange@gmail.com>
2026-04-07 14:10:01 -0700

    wip

--- .gitea/workflows/publish-site.sh
+++ .gitea/workflows/publish-site.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
-export HOME=/home/sig
-export PATH="$HOME/go/bin:$PATH"
-. '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
 
-# Configure git to push
-echo "$GH_PRIV_KEY" > /tmp/deploy_key
-chmod 600 /tmp/deploy_key
-export GIT_SSH_COMMAND="ssh -i /tmp/deploy_key -o IdentitiesOnly=yes"
 
-git config --local user.email "sig@picklerick.it"
-git config --local user.name "sig"
 
 echo "git branch:"
 git branch
@@ -34,10 +25,7 @@ git remote -v
 ymd="$(date +%Y-%m-%d)"
 
 
-git commit -a -m" automated commit on $ymd"
 
-git pull upstream master
-git push upstream master
 
 # Cleanup
 rm -f /tmp/deploy_key
