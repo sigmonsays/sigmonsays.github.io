4de77e0190cc33b1c3b4c458bf3f8f4d9ac9ac3d
Sig Lange <sig.lange@gmail.com>
2026-04-07 13:50:08 -0700

    wip

--- .gitea/workflows/publish-site.sh
+++ .gitea/workflows/publish-site.sh
@@ -4,10 +4,11 @@ export HOME=/home/sig
 export PATH="$HOME/go/bin:$PATH"
 . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
-set -x
-#which go
-#pwd
-#ls -la
+
+# Configure git to push
+echo "$GH_PRIV_KEY" > /tmp/deploy_key
+chmod 600 /tmp/deploy_key
+export GIT_SSH_COMMAND="ssh -i /tmp/deploy_key -o IdentitiesOnly=yes"
 
 # Compile
 go install ./website/
@@ -15,10 +16,6 @@ go install ./website/
 # Generate
 make gen
 
-# Configure git
-echo "$GH_PRIV_KEY" > /tmp/deploy_key
-chmod 600 /tmp/deploy_key
-export GIT_SSH_COMMAND="ssh -i /tmp/deploy_key -o IdentitiesOnly=yes"
 
 # configure remote
 if ! git remote get-url upstream ; then
@@ -30,5 +27,7 @@ ymd="$(date +%Y-%m-%d)"
 
 git commit -a -m" automated commit on $ymd"
 
-git remote get-url upstream
 git push upstream
+
+# Cleanup
+rm -f /tmp/deploy_key
