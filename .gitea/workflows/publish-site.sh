2dd334b48c94e054fa579b984024b3bf059a6ac6
Sig Lange <sig.lange@gmail.com>
2026-04-07 13:44:09 -0700

    set GH_PRIV_KEY

--- .gitea/workflows/publish-site.sh
+++ .gitea/workflows/publish-site.sh
@@ -15,11 +15,17 @@ go install ./website/
 # Generate
 make gen
 
+# Configure git
+echo "$GH_PRIV_KEY" > /tmp/deploy_key
+chmod 600 /tmp/deploy_key
+export GIT_SSH_COMMAND="ssh -i /tmp/deploy_key -o IdentitiesOnly=yes"
+
 # configure remote
 if ! git remote get-url upstream ; then
     git remote add upstream git@github.com:sigmonsays/sigmonsays.github.io
     git push --all upstream
 fi
+
 
 git remote get-url upstream
 git push upstream
