8feda7dd3c01506ff3eab48fc8bfbd1ac2e470fd
Sig Lange <sig.lange@gmail.com>
2026-04-07 13:36:07 -0700

    changes

--- .gitea/workflows/publish-site.sh
+++ .gitea/workflows/publish-site.sh
@@ -3,8 +3,15 @@
 export HOME=/home/sig
 . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
-set -x
-which go
-pwd
-ls -la
+#set -x
+#which go
+#pwd
+#ls -la
+
+# Compile
 go install ./website/
+
+# Generate
+make gen
+
+# Push
